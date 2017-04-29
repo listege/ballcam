23818ac8e15165003bfe29e0eaa3ee4534cb8a16
Listege <djtaka@gmail.com>
2017-04-29 14:57:41 +0900

    * 시작!

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public bool isPlaying = false;
+
+	public float clamp = 0.1f;
+	public float amplitude = 0.3f;
+
+	Rigidbody rbd;
+	float x;
+	float y;
+
+	// Use this for initialization
+	void Start () {
+		rbd = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!isPlaying)
+			return;
+
+		Vector3 power = new Vector3(0f, 0f, 0f);
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			power.x =  amplitude;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			power.x = -amplitude;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			power.z = amplitude;	
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			power.z = -amplitude;
+		}
+
+		rbd.AddForce (power);
+	}
+
+	protected int _stickyCount = 0;
+	protected int _slipperyCount = 0;
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.layer == 8)
+			_stickyCount++;
+		else if (collision.gameObject.layer == 9)
+			_slipperyCount++;
+
+		RecalcDrag ();
+	}
+
+	void OnCollisionExit(Collision collision)
+	{
+		if (collision.gameObject.layer == 8)
+			_stickyCount--;
+		else if (collision.gameObject.layer == 9)
+			_slipperyCount--;
+
+		RecalcDrag ();
+	}
+
+	protected void RecalcDrag()
+	{
+		if (_stickyCount > 0)
+			rbd.drag = 6;
+		else if (_slipperyCount > 0)
+			rbd.drag = 0.5f;
+		else
+			rbd.drag = 2;
+	}
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+	public float timer = 5f;
+
+	public Camera Camera1;
+	public Camera Camera2;
+	public BallController b1;
+	public BallController b2;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("ChangeCam", timer, timer);
+	}
+
+	void ChangeCam(){
+		if (Camera1.gameObject.activeSelf) {
+			Camera2.gameObject.SetActive (true);
+			Camera1.gameObject.SetActive (false);
+			b2.isPlaying = false;
+			b1.isPlaying = true;
+		} else if (Camera2.gameObject.activeSelf) {
+			Camera1.gameObject.SetActive (true);
+			Camera2.gameObject.SetActive (false);
+			b2.isPlaying = true;
+			b1.isPlaying = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
