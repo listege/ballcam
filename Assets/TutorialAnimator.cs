3bdac9a50c058f4cb4bc4e0435e0a77e41435cda
fkkcloud <fkkcloud@gmail.com>
2017-04-30 01:34:10 -0700

    artttt

---
+++ Assets/TutorialAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialAnimator : MonoBehaviour {
+
+	RectTransform rect;
+
+	public float Speed = 0.1f;
+
+	bool ready = false;
+
+
+	// Use this for initialization
+	void Start () {
+		rect = GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = rect.transform.position; 
+		pos.y += Speed;
+
+		rect.position = pos;
+
+		if (!ready && rect.position.y > 50f) {
+			ready = true;
+		}
+
+		if (ready && Input.anyKeyDown) {
+
+			Application.LoadLevel (Application.loadedLevel + 1);
+			
+		}
+	}
+}
