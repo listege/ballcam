601ed9f6f0dd24e97fb0076feed63af077145354
fkkcloud <fkkcloud@gmail.com>
2017-04-30 04:04:03 -0700

    tutorial lvl1 fix

--- Assets/Scripts/TutorialLevel1.cs
+++ Assets/Scripts/TutorialLevel1.cs
@@ -11,16 +11,27 @@ public class TutorialLevel1 : MonoBehaviour {
 
 	bool IsTutorialLoaded = false;
 
+	GameState gs;
+
 	// Use this for initialization
 	void Start () {
 		img = GetComponent<Image> ();
+		GameObject go = GameObject.Find ("gamestate");
+		gs = null;
+		if (go)
+			gs = go.GetComponent<GameState> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!GameState.tutoriallvl1played && !IsTutorialLoaded && Time.timeSinceLevelLoad > TutorialTime) {
+		if (gs && !gs.isGameOver && !GameState.tutoriallvl1played && !IsTutorialLoaded && Time.timeSinceLevelLoad > TutorialTime) {
 			img.color = new Color (1f, 1f, 1f, 1f);
 			GameState.tutoriallvl1played = true;
+			IsTutorialLoaded = true;
+		}
+
+		if (IsTutorialLoaded && Time.timeSinceLevelLoad > TutorialTime + 5f) {
+			img.color = new Color (1f, 1f, 1f, 0f);
 		}
 	}
 }
