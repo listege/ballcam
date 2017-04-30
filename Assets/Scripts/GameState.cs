49dfa5a2d233ce497621ea3f51ffab125ffafd3a
Listege <djtaka@gmail.com>
2017-04-30 17:17:27 +0900

    * 엔딩 효과 추가

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -17,6 +17,8 @@ public class GameState : MonoBehaviour
 	[HideInInspector]
 	public Image resetTimerImage = null;
 	[HideInInspector]
+	public Image endingImage = null;
+	[HideInInspector]
 	public AudioSource audioSource = null;
 	[HideInInspector]
 	public float playingTime = 0;
@@ -61,13 +63,13 @@ public class GameState : MonoBehaviour
 				playingTimeText.text = string.Format ("Stage {0}", SceneManager.GetActiveScene ().buildIndex);
 				resetTimerImage = rootObject.transform.FindChild ("ResetImage").GetComponent<Image> ();
 				bestTimeText = rootObject.transform.FindChild ("BestText").GetComponent<Text> ();
-				if (PlayerPrefs.HasKey (SceneManager.GetActiveScene ().name))
-				{
+				if (PlayerPrefs.HasKey (SceneManager.GetActiveScene ().name)) {
 					float bestTime = PlayerPrefs.GetFloat (SceneManager.GetActiveScene ().name);
 					int minutes = (int)(bestTime / 60);
 					int seconds = (int)(bestTime - minutes * 60);
-					bestTimeText.text = string.Format("BEST {0:D2}:{1:D2}", minutes, seconds);
+					bestTimeText.text = string.Format ("BEST {0:D2}:{1:D2}", minutes, seconds);
 				}
+				endingImage = rootObject.transform.FindChild ("ClearImage").GetComponent<Image> ();
 				break;
 			}
 		}
@@ -229,6 +231,7 @@ public class GameState : MonoBehaviour
 			int seconds = (int)(playingTime - minutes * 60);
 			bestTimeText.text = string.Format("BEST {0:D2}:{1:D2}", minutes, seconds);
 		}
+		endingImage.gameObject.SetActive (true);
 	}
 
 	IEnumerator Coroutine_EndingObject()
