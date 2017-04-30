511a4deb4363755e70af146778a24ebe1a8ed857
Jaewon Yoo <djtaka@gmail.com>
2017-04-30 23:39:56 +0900

    * 타이틀 음악 오프닝까지 나오게

---
+++ Assets/Scripts/TitleMusic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleMusic : MonoBehaviour
+{
+    protected int lastScene = -999;
+	void Update ()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentScene != lastScene)
+        {
+            if (currentScene == 1) // Title
+                DontDestroyOnLoad(gameObject);
+            if (currentScene >= 3)
+                Destroy(gameObject);
+            lastScene = SceneManager.GetActiveScene().buildIndex;
+        }
+	}
+}
